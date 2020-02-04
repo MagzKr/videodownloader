76eac03dbbd405382fc2343d7bcc30146bbfd5a4
magz <magzkr@gmail.com>
2020-02-04 22:07:29 +0300

    update

---
+++ downloadthread.py
@@ -0,0 +1,77 @@
+from PyQt5.QtCore import QThread, pyqtSignal
+import requests
+
+class DownloadingThread(QThread):
+    progress_sign = pyqtSignal(int)
+
+    def __init__(self, mainwindow, parent=None):
+        super().__init__()
+        self.mainwindow = mainwindow
+        self.HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": '',
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache"
+}
+        print('hello')
+
+    def getSegsNum(self, m3):
+        """ figure out how many segments there are using the m3u8 file """
+        lines = m3.text.split('\n')
+        self.segs = []
+        for line in lines:
+            if '.ts' in line:
+                self.segs.append(line)
+        return self.segs
+
+    def dumpSegs(self, segments, path):
+        progress = 0
+        """ downlaod and combine the .ts files
+        given the first seg's url, the number of segments and
+        the destination download path """
+        i = 100 / len(segments)
+        for url in segments:
+            with open(path, 'ab') as f:
+                seg = requests.get(url)
+                f.write(seg.content)
+                progress += i
+                self.progress_sign.emit(progress)
+
+    def m3u8_parser(self, url, login='mid97@mail.ru', password='qwerty'):
+        s = requests.Session()
+        login_url = 'https://get.egorarslanov.ru/cms/system/login?required=true'
+        data = {
+            'action': 'processXdget',
+            'xdgetId': '99945',
+            'params[action]': 'login',
+            'params[url]': login_url,
+            'params[email]': login,
+            'params[password]': password,
+            'params[object_type]': 'cms_page',
+            'params[object_id]': '-1',
+            'requestTime': '1580323420',
+            'requestSimpleSign': '0ba52fcf1bee0dace58a5df2c04ff5cc',
+        }
+        s.get(login_url)
+        s.post(login_url, data)
+        r = s.get(url)
+        for i in r.text.splitlines():
+            if '/player/' in i:
+                r = s.get(i[i.find('https'): i.find('"></iframe>')])
+
+        for i in r.text.splitlines():
+            if 'data-master' in i:
+                r = s.get(i[i.find('https'): -1])
+        r = r.text.splitlines()
+        self.m3u8 = requests.get(r[-1])
+        return self.m3u8
+
+    def run(self):
+        self.m3u8 = self.m3u8_parser(self.mainwindow.lineLink.text())
+        self.segments = self.getSegsNum(self.m3u8)
+        self.dumpSegs(self.segments, self.mainwindow.SaveWay.text())
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtCore import QThread
-import time
 import designw
-import download
+import downloadthread
 import os
 
 
@@ -16,13 +15,11 @@ class ExampleApp(QtWidgets.QMainWindow, designw.Ui_VideoDownloader):
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
         self.initUI()
 
-
     def initUI(self):
         self.DownloadButton.clicked.connect(self.startdownloading)
         self.SelWayButton.clicked.connect(self.browse_folder)
         self.SaveWay.insert(os.getcwd() + '/video.mp4')
-        self.DownloadingThread_inst = DownloadingThread(mainwindow=self)
-
+        self.DownloadingThread_inst = downloadthread.DownloadingThread(mainwindow=self)
 
     def browse_folder(self):
             options = QFileDialog.DontUseNativeDialog
@@ -37,23 +34,26 @@ class ExampleApp(QtWidgets.QMainWindow, designw.Ui_VideoDownloader):
 
 
     def startdownloading(self):
-        self.DownloadingThread_inst.start()
+        if self.SaveWay.text() and self.lineLink.text():
+            self.DownloadingThread_inst.progress_sign.connect(self.updateprogressbar)
+            self.ConsoleLog.setPlainText('Видео загружается...')
+            self.DownloadingThread_inst.start()
+            self.ConsoleLog.setPlainText('Загрузка завершена.')
+        else:
+            self.ConsoleLog.setPlainText('Вы не выбрали путь для сохранения файла или не ввели ссылку.')
 
 
-class DownloadingThread(QThread):
-    def __init__(self, mainwindow, parent=None):
-        super().__init__()
-        self.mainwindow = mainwindow
+    def updateprogressbar(self, val):
+        self.ProgressBar.setValue(val)
 
 
-    def run(self):
-        self.mainwindow.ConsoleLog.insertPlainText('Вы не выбрали путь для сохранения файла или не ввели ссылку.')
-        if self.mainwindow.SaveWay.text() and self.mainwindow.lineLink.text():
-             download.start_download(self.mainwindow.SaveWay.text(), self.mainwindow.ConsoleLog,
-                                     self.mainwindow.lineLink.text())
-        # else:
-        #     self.mainwindow.ConsoleLog.setPlainText('Вы не выбрали путь для сохранения файла или не ввели ссылку.')
-
+# class DownloadingThread(QThread):
+#     def __init__(self, mainwindow, parent=None):
+#         super().__init__()
+#         self.mainwindow = mainwindow
+#
+#     def run(self):
+#         download.start_download(self.mainwindow.SaveWay.text(), self.mainwindow.lineLink.text())
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
